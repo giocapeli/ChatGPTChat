2989e01c0592d4f157deab2efee33fce64bd04db
Giovanni Capeli <capeli.andreas@gmail.com>
2023-03-10 20:41:19 +0100

    working

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask import Flask, redirect, render_template, request, url_for
 openai.api_key = key.OPENAI_API_KEY
 app = Flask(__name__)
 
-opener = ['Pretend your talking to a friend so answer his question or make a new question following the conversation.']
+opener = ['Start a conversation or keep up with the subject.']
 history_lines = []
 history_lines_objects = []
 
@@ -16,15 +16,13 @@ def index():
         new_opener = opener.copy()
         topic = request.form["topic"]
         if len(topic) > 0:
-            new_opener.append(f'The starting topic is: {topic}. ')
+            new_opener.append(f'The topic is {topic}. ')
         else:
-            new_opener.append("Start with any topic. ")
+            new_opener.append("There is no main topic. ")
         str_opener = ' '.join(new_opener)
-        new_line(str_opener, "you")
-        new_line(str_opener, "friend")
-        new_line(str_opener, "you")
-        new_line(str_opener, "friend")
-        new_line(str_opener, "you")
+        for n in range(2):
+            new_line(str_opener, "1")
+            new_line(str_opener, "2")
         
         print(history_lines_objects)
     result = ''
@@ -32,23 +30,24 @@ def index():
         result += f'{obj["person"]}: {obj["line"]}\n'
     return render_template("index.html", result=result)
 
-def new_line(prompt, person):
-    for obj in history_lines_objects:
-        if obj['person'] == person:
-            person_line = 'You'
-        else:
-            person_line = 'Friend'
-        prompt += f'{person_line}: {obj["line"]}\n'
-    prompt += f'You:'
-
+def new_line(start_prompt, person):
+    new_prompt = start_prompt
+    new_prompt += f'You are the Person {person}.\n' 
+    if len(history_lines_objects) > 0:
+        for obj in history_lines_objects:
+            new_prompt += f'Person {obj["person"]}: {obj["line"]}\n'
+    new_prompt += f'Person {person}:'
+    print('New prompt:\n'+ new_prompt)
     response = openai.Completion.create(
                 model="text-davinci-003",
-                prompt=prompt,
+                prompt=new_prompt,
                 temperature=0.6,
                 max_tokens=1000
             )
     result = response.choices[0].text
-    time.sleep(3)
+    print('result:\n'+result)
+    
+    time.sleep(1)
     history_lines.append(f'Person {person}: {result}')
     history_lines_objects.append({
         "person": person,
