f6d5e8c7ba91e5bfec3703afb9cf765ef8360463
Giovanni Capeli <capeli.andreas@gmail.com>
2023-03-11 10:46:20 +0100

    chatgpt changes

--- main.py
+++ main.py
@@ -14,48 +14,39 @@ history_lines_objects = []
 def index():
     if request.method == "POST":
         new_opener = opener.copy()
-        topic = request.form["topic"]
-        if len(topic) > 0:
-            new_opener.append(f'The topic is {topic}. ')
+        topic = request.form.get("topic", "")
+        if topic:
+            new_opener.append(f"The topic is {topic}. ")
         else:
             new_opener.append("There is no main topic. ")
-        str_opener = ' '.join(new_opener)
-        for n in range(2):
+        str_opener = " ".join(new_opener)
+        for _ in range(2):
             new_line(str_opener, "1")
             new_line(str_opener, "2")
-        
         print(history_lines_objects)
-    result = []
-    for obj in history_lines_objects:
-        result.append(f'{obj["person"]}: {obj["line"]}')
+    result = [f"{obj['person']}: {obj['line']}" for obj in history_lines_objects]
     return render_template("index.html", result=result)
 
 def new_line(start_prompt, person):
-    new_prompt = start_prompt
-    new_prompt += f'You are the Person {person}.\n' 
-    if len(history_lines_objects) > 0:
-        for obj in history_lines_objects:
-            new_prompt += f'Person {obj["person"]}: {obj["line"]}\n'
+    new_prompt = f"{start_prompt}You are the Person {person}.\n"
+    if history_lines_objects:
+        new_prompt += "".join(f"Person {obj['person']}: {obj['line']}\n" for obj in history_lines_objects)
     else:
-        new_prompt += f'Person 2: Hello, you!\n'
-    new_prompt += f'Person {person}:'
-    print('New prompt:\n'+ new_prompt)
+        new_prompt += "Person 2: Hello, you!\n"
+    new_prompt += f"Person {person}:"
+    print(f"New prompt:\n{new_prompt}")
     response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=new_prompt,
-                temperature=0.6,
-                max_tokens=1000
-            )
+        model="text-davinci-003",
+        prompt=new_prompt,
+        temperature=0.6,
+        max_tokens=1000,
+    )
     result = response.choices[0].text
-    print('result:\n'+result)
-    
+    print(f"Result:\n{result}")
     time.sleep(1)
-    history_lines.append(f'Person {person}: {result}')
-    history_lines_objects.append({
-        "person": person,
-        "line": result.strip()
-    })
+    line = f"Person {person}: {result.strip()}"
+    history_lines.append(line)
+    history_lines_objects.append({"person": person, "line": result.strip()})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-    
