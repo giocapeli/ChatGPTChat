423d1785a04934f57549827223a6cb54ec943da2
Giovanni Capeli <capeli.andreas@gmail.com>
2023-03-11 11:21:03 +0100

    layout

--- main.py
+++ main.py
@@ -3,6 +3,31 @@ import time
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
+testarray = [
+  {
+    "sender": "Alice",
+    "text": "Hey, how are you?",
+    "timestamp": "2022-03-10T13:45:00.000Z",
+    "mine":True
+  },
+  {
+    "sender": "Bob",
+    "text": "I'm good, thanks for asking. How about you?",
+    "timestamp": "2022-03-10T13:46:30.000Z"
+  },
+  {
+    "sender": "Alice",
+    "text": "I'm doing well too, thanks.",
+    "timestamp": "2022-03-10T13:48:15.000Z",
+    "mine":True
+  },
+  {
+    "sender": "Bob",
+    "text": "That's great to hear!",
+    "timestamp": "2022-03-10T13:49:00.000Z"
+  }
+]
+
 openai.api_key = key.OPENAI_API_KEY
 app = Flask(__name__)
 
@@ -24,29 +49,30 @@ def index():
             new_line(str_opener, "1")
             new_line(str_opener, "2")
         print(history_lines_objects)
-    result = [f"{obj['person']}: {obj['line']}" for obj in history_lines_objects]
-    return render_template("index.html", result=result)
+    result = [f"{obj['sender']}: {obj['text']}" for obj in history_lines_objects]
+    return render_template("index.html", messages=testarray)
 
 def new_line(start_prompt, person):
     new_prompt = f"{start_prompt}You are the Person {person}.\n"
     if history_lines_objects:
-        new_prompt += "".join(f"Person {obj['person']}: {obj['line']}\n" for obj in history_lines_objects)
+        new_prompt += "".join(f"Person {obj['sender']}: {obj['text']}\n" for obj in history_lines_objects)
     else:
         new_prompt += "Person 2: Hello, you!\n"
     new_prompt += f"Person {person}:"
-    print(f"New prompt:\n{new_prompt}")
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=new_prompt,
-        temperature=0.6,
-        max_tokens=1000,
-    )
-    result = response.choices[0].text
+    # print(f"New prompt:\n{new_prompt}")
+    # response = openai.Completion.create(
+    #     model="text-davinci-003",
+    #     prompt=new_prompt,
+    #     temperature=0.6,
+    #     max_tokens=1000,
+    # )
+    # result = response.choices[0].text
+    result = "test"
     print(f"Result:\n{result}")
     time.sleep(1)
     line = f"Person {person}: {result.strip()}"
     history_lines.append(line)
-    history_lines_objects.append({"person": person, "line": result.strip()})
+    history_lines_objects.append({"sender": person, "text": result.strip()})
 
 if __name__ == "__main__":
     app.run(debug=True)
